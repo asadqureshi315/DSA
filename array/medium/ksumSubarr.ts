185d576a018ee88af98774955d6b6115ca17c04e
asad315 <84276124+asadqureshi315@users.noreply.github.com>
2025-11-03 22:28:00 +0530

    Update ksumSubarr.ts

--- array/medium/ksumSubarr.ts
+++ array/medium/ksumSubarr.ts
@@ -1,3 +1,31 @@
+function maxKSumSubArr(nums: number[], k: number): number {
+    let n = nums.length
+    let maxLen = 0
+    let l=0
+    let r=0
+    let sum=0
+
+    // maintain 2 pointers left and right
+    // initailly we just add nums[r] to sum,
+    // if sum==k we just calc maxLen but if sum>k 
+    // then we start another while loop
+    // which runs only if sum>k, it start substracting form left side i.e. nums[l] from sum
+    while(r<n){
+        sum+=nums[r]
+        while(sum>k && l<r){
+            sum-=nums[l]
+            l++
+        }
+        if(sum==k){
+            maxLen=Math.max(maxLen, r-l+1)
+        }
+        r++
+    }
+    return maxLen
+}
+console.log(maxKSumSubArr([2, 3, 5, 1, 9], 10))
+console.log(maxKSumSubArr([1, 1, 2, 3], 3))
+
 function subarraySum(arr: number[], k: number): number {
   let n = arr.length;
   let hsh = new Map();
@@ -20,3 +48,4 @@ function subarraySum(arr: number[], k: number): number {
   return c;
 }
 subarraySum([1, 2, 3], 3);
+
