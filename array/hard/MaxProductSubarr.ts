7ce0fde3f567d1ab43da5f67b38a80e54240d4c1
asad315 <84276124+asadqureshi315@users.noreply.github.com>
2025-11-11 22:27:50 +0530

    Refactor maxProduct to maxProductSubArr with optimization
    Refactored maxProduct function to maxProductSubArr for better performance using prefix and suffix products. Added handling for zeroes to reset calculations.

--- array/hard/MaxProductSubarr.ts
+++ array/hard/MaxProductSubarr.ts
@@ -1,3 +1,23 @@
+// Optimal
+// we will maintain prefix product and suffix product, 
+// when ever we encounter 0 we will reset prefix or suffix to 0 i.e we will need to start re calc after 0 since n*0=0
+// will set maxProduct to max of prefix, suffix and maxPrd
+function maxProductSubArr(nums:number[]){
+    let n=nums.length
+    let prefix=1
+    let suffix=1
+    let maxPrd=-Infinity
+    for(let i=0;i<n;i++){
+        if(prefix==0) prefix=1
+        if(suffix==0) suffix=1
+        prefix*=nums[i]
+        suffix*=nums[n-i-1]
+        maxPrd=Math.max(maxPrd, prefix,suffix)
+    }
+    return maxPrd
+}
+console.log(maxProductSubArr([1,3,-3,2,0,4,-4,8,2,9]))
+
 function maxProduct(nums: number[]): number {
     let n = nums.length
     let maxPrd = nums[0]
@@ -10,3 +30,4 @@ function maxProduct(nums: number[]): number {
     }
     return maxPrd
 };
+
