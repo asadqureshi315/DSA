851338019ff8d90f727b6a30f34b9663d96ceaf2
asad315 <84276124+asadqureshi315@users.noreply.github.com>
2026-03-31 23:12:29 +0530

    Implement iterative Fibonacci function
    Added an iterative version of the Fibonacci function for optimization.

--- recursion/finbonacci.ts
+++ recursion/finbonacci.ts
@@ -5,4 +5,17 @@ function fibonacci(n: number): number {
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
+// iterative, very optimized
+function fib(n: number): number {
+    if(n<=1) return n
+    let a=0
+    let b=1
+    for(let i=2;i<=n;i++){
+        let fibo=a+b
+        a=b
+        b=fibo
+    }
+    return b
+};
+
 console.log(fibonacci(5));
